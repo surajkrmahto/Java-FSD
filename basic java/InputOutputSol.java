d0a6de6a6817c9ffe413d9956db80ec25d109305
Suraj Kumar Mahto <swarajsuraj786@gmail.com>
2023-11-09 14:50:43 +0530

    que solved

---
+++ basic java/InputOutputSol.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class InputOutputSol{
+    
+    public static void main(String[] args) {
+       //Write your code here
+        Scanner scanner = new Scanner(System.in);
+        int myInt = scanner.nextInt();
+        int myInt1 = scanner.nextInt();
+        int myInt2 = scanner.nextInt();
+        System.out.println( myInt);
+        System.out.println( myInt1);
+        System.out.println( myInt2);
+    }
+}
